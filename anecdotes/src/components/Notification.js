8be969156efb50d9d9da34bed34c592cf63abed8
quincinia <eanj13@gmail.com>
2022-02-04 21:59:26 -0800

    Notification will not render a null message

--- anecdotes/src/components/Notification.js
+++ anecdotes/src/components/Notification.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Notification = () => {
-    const notification = useSelector((state) => state.message)
+    const message = useSelector((state) => state.message)
+    if (message === null) {
+        return null
+    } 
     const style = {
         border: 'solid',
         padding: 10,
         borderWidth: 1
     }
-    return <div style={style}>{notification}</div>
+    return <div style={style}>{message}</div>
 }
 
 export default Notification
