e790735479dc007b36a3250fa9b1ad7f444b3f26
quincinia <eanj13@gmail.com>
2022-02-08 21:51:21 -0800

    Added POST request

--- anecdotes/src/services/anecdotes.js
+++ anecdotes/src/services/anecdotes.js
@@ -7,4 +7,9 @@ const getAll = async () => {
     return res.data
 }
 
-export default { getAll }
+const createNew = async (content) => {
+    const res = await axios.post(baseUrl, { content, votes: 0 })
+    return res.data
+}
+
+export default { getAll, createNew }
